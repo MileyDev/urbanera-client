df2edad2a15e05f055db3c94ed44fed992792be0
Micheal Sokoya <151823022+MileyDev@users.noreply.github.com>
2025-10-10 21:47:30 +0100

    polishes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,21 +10,21 @@ export default function NavBar() {
     <nav className="navbar navbar-expand-lg sticky-top" style={{ background: 'var(--dark-gold)' }}>
       <div className="container">
         <Link className="navbar-brand fw-bold" to="/" style={{ color: 'var(--black)' }}>UrbanEra</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon" style={{ color: 'white'}}></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" style={{ border: 'none'}}>
+          <span className="navbar-toggler-icon" ></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/shop" style={{ color: 'var(--white)', transition: 'color 0.3s ease'}}>Shop <i className="bi bi-bag"></i></Link>
+              <Link className="nav-link" to="/shop" style={{ color: 'var(--black)', transition: 'color 0.3s ease'}}>Shop <i className="bi bi-bag"></i></Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/cart" style={{ color: 'var(--white)', transition: 'color 0.3s ease'}}>
+              <Link className="nav-link" to="/cart" style={{ color: 'var(--black)', transition: 'color 0.3s ease'}}>
                 Cart <i className="bi bi-cart"></i>{itemCount > 0 && <span className="badge bg-light text-dark ms-1">{itemCount}</span>}
               </Link>             
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/magazine" style={{ color: 'var(--white)', transition: 'color 0.3s ease'}}>
+              <Link className="nav-link" to="/magazine" style={{ color: 'var(--black)', transition: 'color 0.3s ease'}}>
                The Urban LookBook <i className="bi bi-book-half"></i>
               </Link>
             </li>
